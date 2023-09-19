593de4652bc65554b3a7e8362a7a5ca9c5105b19
tritan89 <61888066+tritan89@users.noreply.github.com>
2023-09-19 08:50:46 -0700

    fixed button

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,13 +9,13 @@ const projects = () => {
         <Hero heading = "My Projects"  message='' showArrow={true}/>
        
         <Navbar/>
-
+        <div id="bottom" className=''> </div>
         <Post1 heading = "Buffalo" content = 'Buffalo is the latest project ive been working on. It is a meal planning app built using React Native for the frontend and Firebase for the backend. Check out the code base on my github!' 
        link = "https://github.com/tritan89/buffalo" linkText = "Buffalo on Github"/>
-      
+        
         <Post1 heading = "BattleSnake" content = 'This project was done during a hackathon by myself and Carson Jennings on March 11, 2023. During this competition we successfully implemented an snake that could navigate to collect food and avoid going head to head with snakes of equal or greater length. You can read the full write up on my github!' 
        link = "https://github.com/tritan89/snakey" linkText = "Battlesnake Writeup"/>
-
+        
        
        
       
---
+++ pages/todo.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Hero from '@/components/Hero'
+import Navbar from '@/components/Navbar'
+import Form from '@/components/Form'
+
+
+const todo = () => {
+  return (
+    <div>
+        
+   
+        
+        <div className='p-20'><Form/></div>
+    
+    
+    
+    
+</div>
+  )
+}
+
+export default todo
